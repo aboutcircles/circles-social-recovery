02031f397d0e022f69aa8c7d54394bd00d0daa27
roleengineer <roleengineer@gmail.com>
2026-04-14 11:56:41 +0200

    gnosis chain deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,8 +5,8 @@ import {Script} from "forge-std/Script.sol";
 import {SocialRecoveryModule} from "src/SocialRecoveryModule.sol";
 
 contract DeployScript is Script {
-    SocialRecoveryModule public module;
-    address deployer = 0x77e2b886ED1dDc826F04F9835BA08DA3FefaA40B;
+    SocialRecoveryModule public module; // 0x9e80f134f2D2ECEF84D6886827bbc6F06F0e5A56
+    address deployer = 0xedEd14C7b5ac8B2adA30D70B61E1a64CAA37f6c9;
 
     uint256 minimumCooldown = 7 days;
 
