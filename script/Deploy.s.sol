cacf1c56cedfb50f0617a03b8d9b4aded0a6d166
zengzengzenghuy <tayzh2799@gmail.com>
2026-04-29 22:36:32 +0200

    feat: add audit report and update deployment address

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,8 +5,9 @@ import {Script} from "forge-std/Script.sol";
 import {SocialRecoveryModule} from "src/SocialRecoveryModule.sol";
 
 contract DeployScript is Script {
-    SocialRecoveryModule public module; // 0x9e80f134f2D2ECEF84D6886827bbc6F06F0e5A56
-    address deployer = 0xedEd14C7b5ac8B2adA30D70B61E1a64CAA37f6c9;
+    SocialRecoveryModule public module; // 0x3f0fF6225A17FB2bC4d3b3B834c1D29b62AC6555
+    address deployer = 0x4F43446a48292726a381249423EE126CaBfd36B4;
+
 
     uint256 minimumCooldown = 7 days;
 
