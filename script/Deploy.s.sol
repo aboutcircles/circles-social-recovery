367d2bcb97d9107b783ec847ee4b8addfb8428ab
zengzengzenghuy <tayzh2799@gmail.com>
2026-04-29 22:38:52 +0200

    forge fmt

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,7 +8,6 @@ contract DeployScript is Script {
     SocialRecoveryModule public module; // 0x3f0fF6225A17FB2bC4d3b3B834c1D29b62AC6555
     address deployer = 0x4F43446a48292726a381249423EE126CaBfd36B4;
 
-
     uint256 minimumCooldown = 7 days;
 
     function setUp() public {}
